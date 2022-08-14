5dfea67072d90c54d1919f14cbe9a94f2b26412a
Sebastian Vasquez <sebvas18@gmail.com>
2022-08-14 04:28:42 -0500

    3D error

--- main.cpp
+++ main.cpp
@@ -6,20 +6,103 @@
 #include <iostream>
 #include <math.h>
 #include "./src/map.cpp"
-// #include "./src/settings2D.cpp"
+#include "./src/cells/cell.cpp"
+#include "./src/colors/colors.cpp"
 #include <cstdlib>
 #include <unistd.h>
 using namespace std;
 
+bool leftMouseButtonDown = false;
+bool rightMouseButtonDown = false;
+int mouseXPos = 0;
+int mouseYPos = 0;
+double cameraY = 0;
+double cameraAngle = 0;
+double cameraZoom = 10;
 bool pauseGame = true;
 int window1;
 int window2;
 GameMap map;
 
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-5.0, 5.0, -5.0, 5.0, 1.5, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void Linea(float *X0, float *X1, float *C, float A = 1, int f = 1, int P = 0)
+{
+    glColor3fv(C);
+    glLineWidth(A);
+    glEnable(GL_LINE_STIPPLE);
+    switch (P)
+    {
+    case 1:
+        glLineStipple(f, 0x0101);
+        break;
+    case 2:
+        glLineStipple(f, 0xAAAA);
+        break;
+    case 3:
+        glLineStipple(f, 0x00FF);
+        break;
+    case 4:
+        glLineStipple(f, 0x0c0F);
+        break;
+    case 5:
+        glLineStipple(f, 0x1C47);
+        break;
+    default:
+        glLineStipple(f, 0xFFFF);
+        break;
+    }
+    glBegin(GL_LINES);
+    glVertex3fv(X0);
+    glVertex3fv(X1);
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+    glLineWidth(1);
+}
+
+void Ejes(int c)
+{
+    float P[] = {-20.0, 0.0, 0.0}, P1[] = {20.0, 0.0, 0.0};
+    float P2[] = {0.0, -20.0, 0.0}, P3[] = {0.0, 20.0, 0.0};
+    float P4[] = {0.0, 0.0, -20.0}, P5[] = {0.0, 0.0, 20.0};
+    float C1[] = {1, 0, 0}, C2[] = {0, 0, 1}, C3[] = {0, 1, 0};
+    switch (c)
+    {
+    case 0:
+    {
+        Linea(P, P1, C1, 2, 2, 3);
+        Linea(P2, P3, C2, 2, 2, 3);
+        Linea(P4, P5, C3, 2, 2, 3);
+        break;
+    }
+    default:
+    {
+        Linea(P, P1, C1, c);
+        Linea(P2, P3, C2, c);
+        Linea(P4, P5, C3, c);
+    }
+    }
+}
+
 void init()
 {
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(-200, 200, -200, 200);
+}
+
+void drawString(float x, float y, float z, char *string) {
+  glRasterPos3f(x, y, z);
+
+  for (char* c = string; *c != '\0'; c++) {
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+  }
 }
 
 void drawSquare(float x, float y, float lado)
@@ -43,7 +126,7 @@ void drawLines(float x, float y, float lado)
     glEnd();
 }
 
-void display(void)
+void display2D(void)
 {
     glClearColor(0.1, 0.3, 0.1, 0);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -64,6 +147,25 @@ void display(void)
             drawLines(-200 + (j * 20), 200 - (i * 20), 20);
         }
     }
+    // drawString(0.0, 0.0, 0.0, "Esto es una prueba");
+    glutSwapBuffers();
+}
+
+void display3D(void)
+{
+    glClearColor(0, 0, 0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    double cameraX = cos(cameraAngle) * cameraZoom;
+    double cameraZ = sin(cameraAngle) * cameraZoom;
+    gluLookAt(cameraX, cameraY, cameraZ, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    // Ejes(3);
+
+    glPushMatrix();
+    glScalef(0.5, 0.5, 0.5);
+    Cell(red);
+    glPopMatrix();
+    glFlush();
     glutSwapBuffers();
 }
 
@@ -104,7 +206,7 @@ void key(unsigned char key, int x, int y)
     glutPostRedisplay();
 }
 
-void mouse(int button, int state, int x, int y)
+void mouse2D(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
@@ -119,36 +221,72 @@ void mouse(int button, int state, int x, int y)
     glutPostRedisplay();
 }
 
+void mouse3D(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON)
+    {
+        leftMouseButtonDown = (state == GLUT_DOWN);
+    }
+    else if (button == GLUT_RIGHT_BUTTON)
+    {
+        rightMouseButtonDown = (state == GLUT_DOWN);
+    }
+    else if (button == 3)
+    {
+        cameraZoom -= 0.1;
+    }
+    else if (button == 4)
+    {
+        cameraZoom += 0.1;
+    }
+
+    glutPostRedisplay();
+}
+
+void mouseMotion3D(int x, int y)
+{
+    if (leftMouseButtonDown)
+    {
+        if (x > mouseXPos)
+            cameraAngle += 0.01;
+        else if (x < mouseXPos)
+            cameraAngle -= 0.01;
+        if (y > mouseYPos)
+            cameraY += 0.1;
+        else if (y < mouseYPos)
+            cameraY -= 0.1;
+    }
+    mouseXPos = x;
+    mouseYPos = y;
+    cout << "x: " << x << " y: " << y << endl;
+    glutPostRedisplay();
+}
+
 int main(int argc, char *argv[])
 {
-    // map.readMapFromFile("./src/gameMap/map.txt");
-    // map.drawMap();
-
-    // for (int i = 0; i < 10; i++){
-    //     system("clear");
-    //     map.updateMapState();
-    //     map.drawMap();
-    //     sleep(1);
-    // }
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
-    glutInitWindowPosition(150, 30);
-    window1 = glutCreateWindow("ventana 2D");
-    init();
-    glutIdleFunc(idle);
-    glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutMouseFunc(mouse);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    // glutInitWindowSize(600, 600);
+    // glutInitWindowPosition(150, 30);
+    // window1 = glutCreateWindow("ventana 2D");
+    // init();
+    // glutIdleFunc(idle);
+    // glutDisplayFunc(display2D);
+    // glutKeyboardFunc(key);
+    // glutMouseFunc(mouse2D);
 
+    glShadeModel(GL_FLAT);
+    glEnable(GL_DEPTH_TEST);
     glutInitWindowSize(600, 600);
     glutInitWindowPosition(150, 30);
     window2 = glutCreateWindow("ventana 3D");
-    init();
-    glutDisplayFunc(display);
+    // init();
+    glutDisplayFunc(display3D);
     glutIdleFunc(idle);
+    glutReshapeFunc(reshape);
     glutKeyboardFunc(key);
-    glutMouseFunc(mouse);
+    glutMouseFunc(mouse3D);
+    glutMotionFunc(mouseMotion3D);
 
     glutMainLoop();
 
